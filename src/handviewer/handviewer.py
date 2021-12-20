b5c3babd0d27bb5b2160cb5c6e0c44ae49d56133
Phillip Martin <sarabandoid@gmail.com>
2021-12-20 11:27:28 -0500

    autosave changes were missed

--- src/handviewer/handviewer.py
+++ src/handviewer/handviewer.py
@@ -12,53 +12,55 @@ import inputdeal
 import json
 import parseurl
 import re
+import sys
 
 
-class HandViewer:
-    def __init__(self):
-        parser = argparse.ArgumentParser(description = 'Handviewer Tool', )
-        parser.add_argument('input', help='html string, * for console input, or ** for previous deal ')
-        parser.add_argument('-n', '--north', action='store_true', help='print North hand')
-        parser.add_argument('-e', '--east', action='store_true', help='print East hand')
-        parser.add_argument('-s', '--south', action='store_true', help='print South hand')
-        parser.add_argument('-w', '--west', action='store_true', help='print West hand')
-        parser.add_argument('-a', '--auction', action='store_true', help='print auction')
-        parser.add_argument('-r', '--rotate', type=int, help='number of seats to rotate clockwise')
-        parser.add_argument('-o', '--output', default='output', help='common prefix for json and html output files')
-        self._args = parser.parse_args()
+def parse_args(argv):
+    parser = argparse.ArgumentParser(description = 'Handviewer Tool', )
+    parser.add_argument('input', help='html string, * for console input, or ** for previous deal ')
+    parser.add_argument('-n', '--north', action='store_true', help='print North hand')
+    parser.add_argument('-e', '--east', action='store_true', help='print East hand')
+    parser.add_argument('-s', '--south', action='store_true', help='print South hand')
+    parser.add_argument('-w', '--west', action='store_true', help='print West hand')
+    parser.add_argument('-a', '--auction', action='store_true', help='print auction')
+    parser.add_argument('-r', '--rotate', type=int, help='number of seats to rotate clockwise')
+    parser.add_argument('-o', '--output', default='output', help='common prefix for json and html output files')
+    return parser.parse_args(argv)
+ 
+
+def main(args):
+    assert '.' not in args.output, "Output file name should be prefix only" 
     
-    def __call__(self):
-        assert '.' not in self._args.output, "Output file name should be prefix only" 
-        
-        deal = {}
-        
-        # build deal
-        if self._args.input == '**':
-            saveFile = open(self._args.output + ".json", "r")
-            deal = json.load(saveFile)
-            saveFile.close()
-        else:
-            saveFile = open(self._args.output + ".json", "w")
-            if self._args.input == '*':
-                deal = inputdeal.inputDeal()
-                json.dump(deal, saveFile)
-            elif re.match("http", self._args.input):
-                deal = parseurl.parse(self._args.input)
-                json.dump(deal, saveFile)
-            saveFile.close()
-         
-        assert deal, 'Input must be *, **, or start with http'
-        
-        # build the html
-        html = buildhtml.build(deal, self._args)
-       
-        # write it to the specified file
-        f = open(self._args.output + ".html", 'w')
-        f.write(html)
-        f.close()
-        
-        print(f"Html has been written to {self._args.output}")
+    deal = {}
+    
+    # build deal
+    if args.input == '**':
+        saveFile = open(args.output + ".json", "r")
+        deal = json.load(saveFile)
+        saveFile.close()
+    else:
+        saveFile = open(args.output + ".json", "w")
+        if args.input == '*':
+            deal = inputdeal.inputDeal()
+            json.dump(deal, saveFile)
+        elif re.match("http", args.input):
+            deal = parseurl.parse(args.input)
+            json.dump(deal, saveFile)
+        saveFile.close()
+     
+    assert deal, 'Input must be *, **, or start with http'
+    
+    # build the html
+    html = buildhtml.build(deal, args)
+   
+    # write it to the specified file
+    f = open(args.output + ".html", 'w')
+    f.write(html)
+    f.close()
+    
+    print(f"Html has been written to {args.output}")
+  
     
 if __name__ == '__main__':
-    hv = HandViewer()
-    hv()
+    main(parse_args(sys.argv[1:]))
+
